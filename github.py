c751dc8efbc2d7e3b7e6f67c95966659b8f52f9e
Imran Mohammed <imohammed@zendesk.com>
2017-09-29 12:46:13 +0800

    Add time delay

--- github.py
+++ github.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Firefox()
 driver.get("https://github.com")
 search_field = driver.find_element_by_name('q')
 search_field.send_keys("selenium")
 search_field.send_keys(Keys.RETURN)
+print("waiting 30 mins before doing assertion")
+time.sleep(30)
 assert "repository results" in driver.page_source
 driver.close()
